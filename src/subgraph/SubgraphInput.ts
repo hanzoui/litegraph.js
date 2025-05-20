560d2cb28e977b81b3ada224a6c4acd51304c9a5
filtered <176114999+webfiltered@users.noreply.github.com>
2025-05-20 13:43:03 +1000

    nit - Doc

--- src/subgraph/SubgraphInput.ts
+++ src/subgraph/SubgraphInput.ts
@@ -2,6 +2,17 @@ import type { Point, ReadOnlyRect } from "@/interfaces"
 
 import { SubgraphSlot } from "./SubgraphSlotBase"
 
+/**
+ * An input "slot" from a parent graph into a subgraph.
+ *
+ * IMPORTANT: A subgraph "input" is both an input AND an output.  It creates an extra link connection point between
+ * a parent graph and a subgraph, so is conceptually similar to a reroute.
+ *
+ * This can be a little confusing, but is easier to visualise when imagining editing a subgraph.
+ * You have "Subgraph Inputs", because they are coming into the subgraph, which then connect to "node inputs".
+ *
+ * Functionally, however, when editing a subgraph, that "subgraph input" is the "origin" or "output side" of a link.
+ */
 export class SubgraphInput extends SubgraphSlot {
   get labelPos(): Point {
     const [x, y, , height] = this.boundingRect
