84c92fdae8e677ccb6ef64ea224fb916bc22fae6
Benjamin Lu <benjaminlu1107@gmail.com>
2025-07-23 14:16:23 -0400

    Revert shotgun tsc changes

--- src/LGraphCanvas.ts
+++ src/LGraphCanvas.ts
@@ -1289,7 +1289,7 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
 
     input.focus()
 
-    let dialogCloseTimer: ReturnType<typeof setTimeout>
+    let dialogCloseTimer: number
     dialog.addEventListener("mouseleave", function () {
       if (LiteGraph.dialog_close_on_mouse_leave) {
         if (!dialog.is_modified && LiteGraph.dialog_close_on_mouse_leave) {
@@ -6170,7 +6170,7 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
 
     if (this.ds.scale > 1) dialog.style.transform = `scale(${this.ds.scale})`
 
-    let dialogCloseTimer: ReturnType<typeof setTimeout>
+    let dialogCloseTimer: number
     let prevent_timeout = 0
     LiteGraph.pointerListenerAdd(dialog, "leave", function () {
       if (prevent_timeout) return
@@ -6356,7 +6356,7 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
     if (options.hide_on_mouse_leave) {
       // FIXME: Remove "any" kludge
       let prevent_timeout: any = false
-      let timeout_close: ReturnType<typeof setTimeout> | null = null
+      let timeout_close: number | null = null
       LiteGraph.pointerListenerAdd(dialog, "enter", function () {
         if (timeout_close) {
           clearTimeout(timeout_close)
@@ -6401,7 +6401,7 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
     that.search_box = dialog
 
     let first: string | null = null
-    let timeout: ReturnType<typeof setTimeout> | null = null
+    let timeout: number | null = null
     let selected: ChildNode | null = null
 
     const maybeInput = dialog.querySelector("input")
@@ -6996,7 +6996,7 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
       }
     }
 
-    let dialogCloseTimer: ReturnType<typeof setTimeout>
+    let dialogCloseTimer: number
     let prevent_timeout = 0
     dialog.addEventListener("mouseleave", function () {
       if (prevent_timeout) return
