56eba7dbc9473be045ba96003730b66fe9179838
Benjamin Lu <benjaminlu1107@gmail.com>
2025-07-23 14:02:18 -0400

    Update test path

--- test/LGraphNodeProperties.test.ts
+++ test/LGraphNodeProperties.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from "vitest"
 
-import { LGraphNodeProperties } from "./LGraphNodeProperties"
+import { LGraphNodeProperties } from "@/LGraphNodeProperties"
 
 describe("LGraphNodeProperties", () => {
   let mockNode: any
